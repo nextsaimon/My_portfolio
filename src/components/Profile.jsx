4ec1600472f82af8e3fa5a9cd97b4999c7129e55
Saimon <saimonss5432@gmail.com>
2025-10-06 15:01:06 +0600

    ok

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -7,41 +7,6 @@ import AnimatedContent from "@/styles/AnimatedContent/AnimatedContent";
 import FadeContent from "@/styles/FadeContent/FadeContent";
 
 const Profile = () => {
-  const m_b1_1 = () => {
-    const btn1 = document.getElementById("btn1");
-    btn1.style.color = "white";
-    btn1.style.background = "black";
-    const btn2 = document.getElementById("btn2");
-    btn2.style.color = "black";
-    btn2.style.background = "white";
-  };
-
-  const m_b1_2 = () => {
-    const btn1 = document.getElementById("btn1");
-    btn1.style.color = "black";
-    btn1.style.background = "white";
-    const btn2 = document.getElementById("btn2");
-    btn2.style.color = "white";
-    btn2.style.background = "black";
-  };
-
-  const m_b2_1 = () => {
-    const btn2 = document.getElementById("btn2");
-    btn2.style.color = "black";
-    btn2.style.background = "white";
-    const btn1 = document.getElementById("btn1");
-    btn1.style.color = "white";
-    btn1.style.background = "black";
-  };
-
-  const m_b2_2 = () => {
-    const btn2 = document.getElementById("btn2");
-    btn2.style.color = "white";
-    btn2.style.background = "black";
-    const btn1 = document.getElementById("btn1");
-    btn1.style.color = "black";
-    btn1.style.background = "white";
-  };
   const handleNavClick = (targetId) => {
     // Scroll to target element after a short delay
     setTimeout(() => {
@@ -138,8 +103,6 @@ const Profile = () => {
             <button
               className="btn btn-color-1"
               id="btn1"
-              onMouseOver={m_b1_1}
-              onMouseOut={m_b1_2}
               onClick={() => handleNavClick("about")}
             >
               About Me
@@ -160,8 +123,6 @@ const Profile = () => {
             <button
               className="btn btn-color-2"
               id="btn2"
-              onMouseOver={m_b2_1}
-              onMouseOut={m_b2_2}
               onClick={() => handleNavClick("contact")}
             >
               Contact Info
