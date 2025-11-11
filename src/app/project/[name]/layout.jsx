08768474347b57dbc02aa271209951aae79f1397
Saimon <saimonss5432@gmail.com>
2025-11-11 11:52:24 +0600

    ok

--- src/app/project/[name]/layout.jsx
+++ src/app/project/[name]/layout.jsx
@@ -1,19 +1,17 @@
 import projectData from "@/project_data.json";
 
 export async function generateMetadata({ params }) {
-  // Find the project data based on the URL parameter
   const project = projectData.find((p) => p.name === params.name);
 
   if (project) {
     return {
       title: project.title,
       icons: {
-        icon: `/${project.img}`, // The path to your image in the public folder
+        icon: `/${project.img}`,
       },
     };
   }
 
-  // Optionally, return a default title and icon if the project is not found
   return {
     title: "Project Not Found",
   };
